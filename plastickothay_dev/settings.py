e9738465f552c232896222ee4d3b2db154ee5cc1
mdrayhan03 <phylosopherhossain2022@gmail.com>
2025-05-17 01:47:12 +0600

    ready to deploy

--- plastickothay_dev/settings.py
+++ plastickothay_dev/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')  # Your project-specific static files
+    os.path.join(BASE_DIR, 'assets')  # Your project-specific static files
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type

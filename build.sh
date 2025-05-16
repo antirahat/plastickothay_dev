b6985ab6cc3298bbdc101c0587ea08756e793318
mdrayhan03 <phylosopherhossain2022@gmail.com>
2025-05-17 01:26:11 +0600

    ready to deploy

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "Running migrations..."
-python manage.py migrate --noinput
+# echo "Running migrations..."
+# python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
